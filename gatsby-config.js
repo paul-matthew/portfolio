58c54779211c52791bba9eb900bf9aa4d2c007de
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2022-10-31 23:18:01 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
+    siteTitle: `PM`,
+    siteTitleAlt: `Paul Matthew - Portfolio`,
     siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
     siteUrl: `https://jodie.lekoarts.de`,
     siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Logo from './Logo.png';
+import Logo from './logo.png';
 export default class Header extends Component {
   render() {
     return (
