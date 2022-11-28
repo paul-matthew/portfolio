2293847aa0773ccdb067bdb98b2d450fa50d13d0
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2022-11-28 00:25:31 -0500

    update

--- src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
+++ src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
@@ -1,3 +1,5 @@
-const modifyGrid = (data) => data
+import { filterBySlug } from "@lekoarts/gatsby-theme-jodie/src/utils/resolver-templates";
 
-export default modifyGrid
+const modifyGrid = (data) => filterBySlug(data, ["/about"]);
+
+export default modifyGrid;
