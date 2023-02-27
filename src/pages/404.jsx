f232c1f1c7f2053516ba48806d51cb1e66b18b2c
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2023-02-26 20:44:27 -0500

    Baseline 2.11
    Update to 404 error page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,11 +4,11 @@ import Layout from "@lekoarts/gatsby-theme-jodie/src/components/layout"
 const styles = {
   image: {
     textAlign: 'center',
-    backgroundImage:"url('https://www.impactbnd.com/hubfs/404-error-page-examples-best.jpg')",
+    backgroundImage:"url('https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif')",
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    width: '100vw',
-    height: '100vh'
+    width: '50vw',
+    height: '50vh'
   }
 };
 
