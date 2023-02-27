2abe00cbb26353d23a8a517076393e6075f80c39
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2023-02-26 20:29:35 -0500

    Baseline 2.09
    404 error..background image 2nd attempt

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,11 @@ import Layout from "@lekoarts/gatsby-theme-jodie/src/components/layout"
 const styles = {
   image: {
     textAlign: 'center',
-    backgroundImage:"url('https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif')"
+    backgroundImage:"url('https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif')",
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh'
   }
 };
 
@@ -12,7 +16,7 @@ const NotFound = () => (
   <Layout>
     <div style={styles.image}>
       <h1>404</h1>
-      <p>Page not found.</p>
+      <p>Page was not found.</p>
     </div>
   </Layout>
 )
