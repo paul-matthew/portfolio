09f079980ebe0283bd4b568a2a400478a933d0d6
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2023-02-26 20:18:04 -0500

    Baseline 4.07
    Styling 404 error page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,16 @@
 import * as React from "react"
 import Layout from "@lekoarts/gatsby-theme-jodie/src/components/layout"
 
+const styles = {
+  image: {
+    textAlign: 'center',
+    backgroundImage:"url('./404.gif')"
+  }
+};
+
 const NotFound = () => (
   <Layout>
-    <div style={{ textAlign: `center` }}>
+    <div style={styles.image}>
       <h1>404</h1>
       <p>Page not found.</p>
     </div>
