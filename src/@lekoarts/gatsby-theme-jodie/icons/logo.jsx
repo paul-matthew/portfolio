c96f1b0be1e3cbf4a8347d914b637ff6bd6b92f3
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2023-04-02 17:29:50 -0400

    Baseline 3.01
    Update to logo size based on screen size

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import Logo from './Logo.png';
+
 export default class Header extends Component {
   render() {
+    let imgWidth, imgHeight;
+
+    if (window.innerWidth >= 700) {
+      imgWidth = 150;
+      imgHeight = 150;
+    } else if (window.innerWidth >= 361) {
+      imgWidth = 65;
+      imgHeight = 65;
+    } else {
+      imgWidth = 50;
+      imgHeight = 50;
+    }
+
     return (
       <div className="row">
         <div className="Logo">
-          <img src={Logo} width="125" height="125" />
+          <img
+            src={Logo}
+            width={imgWidth}
+            height={imgHeight}
+          />
         </div>
       </div>
     );
   }
-} 
+}
