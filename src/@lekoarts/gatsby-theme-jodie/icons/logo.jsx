bd20be712039b1992f696a1eba54ec0ee07000a5
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2023-04-02 23:58:57 -0400

    Baseline 3.04
    Logo responsive design issue..attempted fix

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import Logo from './Logo.png';
 
 export default class Header extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imgWidth: 50,
+      imgHeight: 50
+    };
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions = () => {
     let imgWidth, imgHeight;
 
-    if (typeof window !== 'undefined' && window.innerWidth >= 700) {
+    if (window.innerWidth >= 700) {
       imgWidth = 150;
       imgHeight = 150;
-    } else if (typeof window !== 'undefined' && window.innerWidth >= 361) {
+    } else if (window.innerWidth >= 361) {
       imgWidth = 65;
       imgHeight = 65;
     } else {
@@ -16,14 +34,19 @@ export default class Header extends Component {
       imgHeight = 50;
     }
 
+    this.setState({
+      imgWidth,
+      imgHeight
+    });
+  };
+
+  render() {
+    const { imgWidth, imgHeight } = this.state;
+
     return (
       <div className="row">
         <div className="Logo">
-          <img
-            src={Logo}
-            width={imgWidth}
-            height={imgHeight}
-          />
+          <img src={Logo} width={imgWidth} height={imgHeight} />
         </div>
       </div>
     );
