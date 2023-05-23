ad487cd70220447c4e0fd21f1d871c7a964f0903
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2023-05-22 20:45:24 -0400

    Baseline 3.28
    siteImage update

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     siteHeadline: `Portfolio`,
     siteUrl: `https://pmday-break.netlify.app/`,
     siteDescription: `The professional services of PM Daybreak Designs`,
-    siteImage: `/about-us-cover-image6.jpg`,
+    siteImage: `/about-us-cover-image7.jpg`,
     author: `Paul-Matthew`,
   },
   plugins: [
